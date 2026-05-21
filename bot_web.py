8b69f5d0bfb38e15b742b2ce5f98733fb4272e71
Jeff <jeffreyjaijo5@gmail.com>
2026-05-21 23:56:01 +0530

    Seperated files

--- bot_web.py
+++ bot_web.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import get_weather
 from logic import *
 import os
@@ -20,56 +20,13 @@ def home():
     condition = interpret_weather(code)
     advice = generate_advice(temp, code)
 
-    return f"""
-<html>
-<head>
-    <title>Sentinel Weather</title>
-
-    <meta http-equiv="refresh" content="10">
-
-    <style>
-        body {{
-            background-color: #0f172a;
-            color: white;
-            font-family: Arial;
-            padding: 40px;
-        }}
-
-        .card {{
-            background-color: #1e293b;
-            padding: 20px;
-            border-radius: 15px;
-            width: 300px;
-            box-shadow: 0 0 10px rgba(0,0,0,0.3);
-        }}
-
-        h1 {{
-            color: #38bdf8;
-        }}
-
-        p {{
-            font-size: 18px;
-        }}
-    </style>
-</head>
-
-<body>
-
-    <div class="card">
-        <h1>🌤 Sentinel Weather</h1>
-
-        <p>🌡 Temperature: {temp}°C</p>
-        <p>💨 Wind: {wind} km/h</p>
-        <p>☁ Condition: {condition}</p>
-
-        <hr>
-
-        <p>👉 {advice}</p>
-    </div>
-
-</body>
-</html>
-"""
+    return render_template(
+    "index.html",
+    temp=temp,
+    wind=wind,
+    condition=condition,
+    advice=advice
+)
 
 @app.route('/pi')
 def pi():
